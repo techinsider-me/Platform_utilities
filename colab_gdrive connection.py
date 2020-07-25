6df90eee31238120be495bc1f989083cfb3dc758
techinsider-me <68778384+techinsider-me@users.noreply.github.com>
2020-07-25 18:14:53 +0530

    Create colab_gdrive connection.py

---
+++ colab_gdrive connection.py
@@ -0,0 +1,12 @@
+#import packages
+from google.colab import drive
+#call this to mount the g-drive
+drive.mount("/content/drive")
+
+#Important
+'''
+Then you need to open the link(refer below image) that you will get once you run the above code,
+you need allow the permission and you will get a token that you have to copy and paste it in cell in the colab notebook.
+once this is done then you can ready to import the files from google drive to colab notebook.
+'''
+#reference
